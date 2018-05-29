5c3772bddcafec2ed8e0e125661151720c33bb65
agatarauzer <agatarauzer@gmail.com>
2018-05-29 15:44:49 +0200

    exercise 14.3

---
+++ src/main/java/domain/TaskDto.java
@@ -0,0 +1,14 @@
+package domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TaskDto {
+    private Long id;
+    private String title;
+    private String content;
+
+}
