1cb57efae7c94575c48ccec6a5b6641ca7e8870d
agatarauzer <agatarauzer@gmail.com>
2018-06-04 22:50:44 +0200

    exercise 15.3

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -17,7 +17,17 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTask(final Long id) {
-        return repository.findById(id).orElse(null);
+    public Optional<Task> getTask(final Long id) {
+        return repository.findById(id);
     }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public void deleteTask(final Long id) {
+        repository.deleteById(id);
+    }
+
+
 }
