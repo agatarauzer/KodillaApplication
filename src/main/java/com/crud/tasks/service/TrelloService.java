088694f7e76c14a1b0aaee7b170e8e34e00cd07e
agatarauzer <agatarauzer@gmail.com>
2018-07-25 10:32:01 +0200

    exercise 19.4

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -34,7 +34,7 @@ public class TrelloService {
     public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
         CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
         ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(adminConfig.getAdminMail(), SUBJECT,
-                "new card: " + trelloCardDto.getName() + " has been created on your Trello account", null)));
+                "new card: " + trelloCardDto.getName() + " has been created on your Trello account")));
         return newCard;
     }
 }
--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -25,13 +25,13 @@ public class SimpleEmailServiceTest {
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("test@test.com", "test", "test message", "cc test");
+        Mail mail = new Mail("test@test.com", "test", "test message");
 
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        mailMessage.setCc(mail.getToCc());
+        //mailMessage.setCc(mail.getToCc());
 
         //When
         simpleEmailService.send(mail);
